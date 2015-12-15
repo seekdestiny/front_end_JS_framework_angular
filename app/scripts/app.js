33b08588af970c1ed83f337c45b0ac217e9f37ec
seekdestiny <jifeiqian@gmail.com>
2015-12-14 22:31:54 -0800

    setup form and establish two-way data binding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -146,3 +146,22 @@ angular.module('confusionApp', [])
             $scope.dish = dish;
     }])
 
+	.controller('DishCommentController', ['$scope', function($scope) {
+            
+            //Step 1: Create a JavaScript object to hold the comment from the form
+            
+            $scope.comment = {rating:"5", comment:"", author:"", date:"" };
+
+            $scope.submitComment = function () {
+                
+                //Step 2: This is how you record the date
+                $scope.comment.date = new Date().toISOString();
+                
+                // Step 3: Push your comment into the dish's comment array
+                //$scope.dish.comments.push("Your JavaScript Object holding the comment");
+                
+                //Step 4: reset your form to pristine
+                
+                //Step 5: reset your JavaScript object that holds your comment
+            }
+    }]);
