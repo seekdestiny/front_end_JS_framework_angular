9dcc7c802c9dfe813823ff69c8e01ce2966aba99
seekdestiny <jifeiqian@gmail.com>
2015-12-20 13:07:54 -0800

    updating services by using ngResourse

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,43 +1,37 @@
 'use strict';
 
 angular.module('confusionApp')
-        .constant("baseURL", "http://localhost:3000/")
-
-        .service('menuFactory', ['$http', 'baseURL', function($http, baseURL) {
+        .constant("baseURL", "resource://localhost:3000/")
+        .service('menuFactory', ['$resource', 'baseURL', function($resource, baseURL) {
     
                 this.getDishes = function(){
                     
-                    return $http.get(baseURL+"dishes");
+                    return $resource(baseURL+"dishes/:id",null,
+					{'update': {method: 'PUT'}});
                     
                 };
     
-                this.getDish = function (index) {
-                    
-                    return $http.get(baseURL+"dishes/"+index);
-                };
-    
-	            this.getPromotion = function(index) {
-                    return $http.get(baseURL+"promotions/"+index);
+	            this.getPromotions = function(){
+
+					return $resource(baseURL+"promotions/:id",null,
+					{'update': {method: 'PUT'}});
+
 				};
                 // implement a function named getPromotion
                 // that returns a selected promotion.
-        })
+        }])
 
-        .factory('corporateFactory', ['$http', 'baseURL', function($http, baseURL) {
+        .factory('corporateFactory', ['$resource', 'baseURL', function($resource, baseURL) {
     
             var corpfac = {};
     
             corpfac.getLeaders = function(){
-                    
-                    return $http.get(baseURL+"leadership");
+
+				    return $resource(baseURL+"leadership/:id",null,
+					{'update': {method: 'PUT'}});
                     
             };
     
-            corpfac.getLeader = function (index) {
-                    
-                    return $http.get(baseURL+"leadership/"+index);
-            };
-
 			return corpfac;
     
 
